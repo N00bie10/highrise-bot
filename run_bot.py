4b2ba112ca55c102d071316ebdc811efeaf282aa
N00bie10 <jaganmarimuthu10@gmail.com>
2026-02-12 17:54:32 +0000

    Update run_bot.py

--- run_bot.py
+++ run_bot.py
@@ -1,12 +1,13 @@
-from asyncio import run as asyncio_run
-from highrise.__main__ import BotDefinition, main
+from highrise.__main__ import main
+import asyncio
+import os
 from bot import HighriseRoomBot
 
-ROOM_ID = "693df3b1a855b7d3cd87e0fb"
-API_TOKEN = "59a326bf94f286d0c3649e22e49505c66c134e5e6d57983e90fd9eeca721b20b"
+ROOM_ID = os.getenv("693df3b1a855b7d3cd87e0fb")
+API_TOKEN = os.getenv("59a326bf94f286d0c3649e22e49505c66c134e5e6d57983e90fd9eeca721b20b")
 
-definitions = [
-    BotDefinition(HighriseRoomBot(), ROOM_ID, API_TOKEN)
-]
+definitions = {
+    ROOM_ID: HighriseRoomBot
+}
 
-asyncio_run(main(definitions))
+asyncio.run(main(definitions))
